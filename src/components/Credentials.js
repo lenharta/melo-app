0d2ce864b5c9cc4a833408833557714813427a5c
Andrew Lenhart <andrew.code21@gmail.com>
2022-01-04 20:29:11 -0500

    spotifyAPI-install pt1

---
+++ src/components/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+  
+  return {
+    ClientId: 'process.env.SPOTIFY_CLIENT_ID',
+    ClientSecret: 'process.env.SPOTIFY_CLIENT_SECRET'
+  }
+}
+
+export default Credentials

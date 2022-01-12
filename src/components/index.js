a290f259b3ba38bb37698e1c195341a9b0ae0a57
Andrew Lenhart <andrew.code21@gmail.com>
2022-01-11 21:20:54 -0500

    update API

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Loader } from './Loader'
 export { default as Navbar } from './Navbar'
 export { default as Search } from './Search'
-export { default as Top5 } from './Top5'
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Top5 } from '../components'
 
 const Home = () => {
 
   return (
     <>
-      <Top5 />
+    
     </>
   )
 }

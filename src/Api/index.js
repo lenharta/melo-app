efe1ae986c075fe45f75a9712432aa9dd3386f29
Andrew Lenhart <andrew.code21@gmail.com>
2022-01-12 21:15:47 -0500

    Update index.js

---
+++ src/Api/index.js
@@ -0,0 +1,9 @@
+
+// Deezer Links
+// Top 100 USA Playlist id = 1313621735
+// Top 100 Worldwide Playlist id = 3155776842
+// Tests (freddie Gibbs)
+// bandana id = 101778542
+// crime pays id = 703804172
+
+// Genius
